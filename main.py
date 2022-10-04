09021efad3d56fdff743e84a8cacbd2ea9cf7d5e
vlangen <vilala@utu.fi>
2022-10-04 16:13:02 +0300

    Second commit. This version works already.

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 
 import os, fileinput, glob, sys
 
-#file_location = os.path.join('.','*.html')
-#file_type = ('*.html')
-
 filenames = glob.glob('*.html')
 
+# Confirm that the user wants to process the files:
 print("\nThis is a python app that adds a language link to the sidebar of all HTML files in your working folder.")
 print("Your working folder is:")
 print(os.getcwd())
-print("\nThe following HTML files will be processed:", filenames)
+print("\nThe following HTML files will be immediately processed:", filenames)
 confirming = input('\nDo you want to continue? Press "y" or "Y" to continue or press any other key to cancel\n')
 
 if confirming == "y" or confirming == "Y":
@@ -19,13 +17,16 @@ else:
     print("Process canceled.")
     sys.exit()
 
-#
-# old = '<ul class="summary">'
-# new = '<li> <a href="https://vldesign.kapsi.fi/r/"><i class="fa fa-language fa-fw"></i> Go to Finnish version<span></span></a></li>'
-#
-# for f in filenames:
-#     for line in fileinput.FileInput(files=f, inplace=True):
-#         if old in line:
-#             line += new + os.linesep
-#         print(line, end="")
+# One line of code (that creates the language link) will be added after the following line in the HTML file(s):
+old = '<ul class="summary">'
+
+# The line that will be added to the HTML file(s):
+new = '<li> <a href="https://vldesign.kapsi.fi/r/"><i class="fa fa-language fa-fw"></i> Go to Finnish version<span></span></a></li>'
+
+# Add the new line to all the HTML files in the working folder with the following loop:
+for f in filenames:
+    for line in fileinput.FileInput(files=f, inplace=True):
+        if old in line:
+            line += new + os.linesep
+        print(line, end="")
 
